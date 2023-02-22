eb6c47bc1d6e1dd01f97e08e55afaf195e576c61
Alexart21 <iskander.m.211@gmail.com>
2023-02-22 10:35:24 +0300

    созданы миграции,модели контроллеры

---
+++ database/migrations/2023_02_22_070844_create_order_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Order;
+use App\Models\Product;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->float('price');
+            $table->integer('qty');
+            $table->float('total');
+            $table->foreignIdFor(Order::class)->constrained();
+            $table->foreignIdFor(Product::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_products');
+    }
+};
